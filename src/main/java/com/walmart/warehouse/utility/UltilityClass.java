3a01ce8ce14f98182c670e29757da5e733408028
bilalbari <bilalbari12@yahoo.com>
2019-03-11 23:11:23 +0530

    Updated Distance Calculater

--- src/main/java/com/walmart/warehouse/utility/UltilityClass.java
+++ src/main/java/com/walmart/warehouse/utility/UltilityClass.java
@@ -7,28 +7,28 @@ public class UltilityClass {
 	public static Double findDistanceBetweenTwoShelves(ShelfDO shelf1, ShelfDO shelf2) {
 		if(shelf1 == null || shelf2 == null)
 			return 0.0;
-		Double a1 = shelf1.getLatitude() - shelf1.getShelfGroup().getStartLatitude();
-		Double a2 = shelf1.getLatitude() - shelf1.getShelfGroup().getEndLatitude();
-		Double c1 = shelf2.getLatitude() - shelf2.getShelfGroup().getStartLatitude();
-		Double c2 = shelf2.getLatitude() - shelf2.getShelfGroup().getEndLatitude();
-		Double b1 = shelf1.getLongitude() - shelf1.getShelfGroup().getStartLongitude();
-		Double b2 = shelf1.getLongitude() - shelf1.getShelfGroup().getEndLongitude();
-		Double d1 = shelf2.getLongitude() - shelf2.getShelfGroup().getStartLongitude();
-		Double d2 = shelf2.getLongitude() - shelf2.getShelfGroup().getEndLongitude();
-		Double shelf1path1Latitude = shelf1.getLatitude()+a1;
-		Double shelf1path2Latitude = shelf1.getLatitude()+a2;
-		Double shelf1path1Longitude = shelf1.getLongitude()+b1;
-		Double shelf1path2Longitude = shelf1.getLongitude()+b2;
-		Double shelf2path1Latitude = shelf2.getLatitude()+c1;
-		Double shelf2path2Latitude = shelf2.getLatitude()+c2;
-		Double shelf2path1Longitude = shelf2.getLongitude()+d1;
-		Double shelf2path2Longitude = shelf2.getLongitude()+d2;
-		Double e1,e2,e3,e4;
-		e1 = getManhattanDistance(shelf1path1Latitude, shelf1path1Longitude, shelf2path1Latitude, shelf2path1Longitude)+Math.abs(a1)+Math.abs(b1)+Math.abs(c1)+Math.abs(d1);
-		e2 = getManhattanDistance(shelf1path1Latitude, shelf1path1Longitude, shelf2path2Latitude, shelf2path2Longitude)+Math.abs(a1)+Math.abs(b1)+Math.abs(c2)+Math.abs(d2);
-		e3 = getManhattanDistance(shelf1path2Latitude, shelf1path2Longitude, shelf2path2Latitude, shelf2path2Longitude)+Math.abs(a2)+Math.abs(b2)+Math.abs(c2)+Math.abs(d2);
-		e4 = getManhattanDistance(shelf1path2Latitude, shelf1path2Longitude, shelf2path1Latitude, shelf2path1Longitude)+Math.abs(a2)+Math.abs(b2)+Math.abs(c1)+Math.abs(d1);
-		return Math.min(e1, Math.min(e2, Math.min(e3, e4)));
+		Double P1 = shelf1.getLongitude()-shelf1.getShelfGroup().getStartLongitude();
+		Double P2 = shelf1.getShelfGroup().getEndLongitude()- shelf1.getLongitude();
+
+		Double P3 = shelf2.getLongitude()-shelf2.getShelfGroup().getStartLongitude();
+		Double P4 = shelf2.getShelfGroup().getEndLongitude()- shelf2.getLongitude();
+		Double V1 = P1+P3+getManhattanDistance(shelf1.getShelfGroup().getStartLatitude(),
+												shelf2.getLatitude(),
+												shelf1.getShelfGroup().getStartLongitude(),
+												shelf2.getShelfGroup().getStartLongitude());
+		Double V2 = P1+P4+getManhattanDistance(shelf1.getShelfGroup().getStartLatitude(),
+				shelf2.getLatitude(),
+				shelf1.getShelfGroup().getStartLongitude(),
+				shelf2.getShelfGroup().getEndLongitude());
+		Double V3 = P2+P4+getManhattanDistance(shelf1.getShelfGroup().getStartLatitude(),
+				shelf2.getLatitude(),
+				shelf1.getShelfGroup().getEndLongitude(),
+				shelf2.getShelfGroup().getEndLongitude());
+		Double V4 = P2+P3+getManhattanDistance(shelf1.getShelfGroup().getStartLatitude(),
+				shelf2.getLatitude(),
+				shelf1.getShelfGroup().getEndLongitude(),
+				shelf2.getShelfGroup().getStartLongitude());
+		return Math.min(V1, Math.min(V2, Math.min(V3, V4)));
 	}
 	
 	private static Double getManhattanDistance(double x1, double x2, double y1, double y2) {
